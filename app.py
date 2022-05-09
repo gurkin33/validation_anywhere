fa873b56e821bec46c6b3d2c63155bd31b2fd24a
gurkin33 <ya@gurkin33.ru>
2022-05-09 12:03:10 +0300

    added CORS to allow connections

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request
 from respect_validation import FormValidator as fv, Validator as v
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+CORS(app, allow_headers='*')
 
-@app.route("/")
+
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'GET':
         return "It works!"
@@ -15,9 +18,9 @@ def hello_world():
             "username": v.stringType().alnum().noWhitespace().length(4, 32).Not(
                 v.oneOf(v.equals('admin'), v.equals('root'))),
             "email": v.Optional(v.email()),
-            "first_name": v.Optional(v.length(3, 32).alpha(' ')),
-            "second_name": v.Optional(v.length(3, 32).alpha(' ')),
-            "personal_id": v.IntVal().positive().Min(1),
+            "first_name": v.Optional(v.length(3, 32).alpha(' ')).set_name('First name'),
+            "second_name": v.Optional(v.length(3, 32).alpha(' ')).set_name('Second name'),
+            "personal_id": v.IntVal().positive().Min(1).set_name('Personal ID'),
             "password": v.length(8, 64).Not(
                 v.anyOf(v.lowercase(), v.uppercase(), v.IntVal(), v.alpha(),
                         v.phone(), v.email())).alnum(' ', '!\'"#$%&''()*+,‐./:;<=>?@')
